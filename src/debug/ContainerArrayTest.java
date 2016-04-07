23094e0a4dc88c9b2242da97fe014f9690a28dd4
Arjun Desai <arjunaashna@gmail.com>
2016-04-07 17:15:31 -0400

    Finished lab activity

--- src/debug/ContainerArrayTest.java
+++ src/debug/ContainerArrayTest.java
@@ -15,10 +15,15 @@ public class ContainerArrayTest {
 
     @Test
     public void testSizeChangeWithAdd () {
+        assertEquals("Initial Size",0,myContainer.size());
         myContainer.add("Alligator");
         myContainer.add("Bear");
         myContainer.add("Camel");
         assertEquals("Add size", 3, myContainer.size());
+        for (int i =0;i<100;i++){
+            myContainer.add(Integer.toString(i));
+        }
+        assertEquals("Max size", 10, myContainer.size());
     }
 
     @Test
@@ -30,18 +35,23 @@ public class ContainerArrayTest {
 
     @Test
     public void testSizeChangeWithRemove () {
+        System.out.println("DAS"+myContainer.size());
         myContainer.add("Alligator");
         myContainer.add("Bear");
         myContainer.remove("Alligator");
+
         assertEquals("Remove size", 1, myContainer.size());
     }
 
     @Test
     public void testObjectIsRemoved () {
         String alligator = "Alligator";
-        myContainer.add("Alligator");
+        myContainer.add(alligator);
         myContainer.add("Bear");
         myContainer.remove("Bear");
         assertEquals("Remove should be same reference", alligator, myContainer.get(0));
     }
+    
+    
+    
 }
---
+++ src/voogasalad/Vector.java
@@ -0,0 +1,136 @@
+package voogasalad;
+
+/**
+ * 
+ * @author Aditya Srinivasan, Arjun Desai, Nick Lockett, Harry Guo, Hunter Lee
+ *
+ */
+public class Vector {
+
+	private double xPrev = 0;
+	private double yPrev = 0;
+	private double x;
+	private double y;
+	private double angle = 0;;
+	
+	/**
+	 * Vector class that keeps information about (x, y) coordinates as well as
+	 * previous coordinates
+	 * 
+	 * Whenever current coordinates are updated, previous coordinates are updated
+	 * accordingly
+	 * 
+	 */
+	public Vector (double x, double y) {
+		setXY(x, y);
+	}
+
+	/**
+	 * Get the current x coordinate
+	 * 
+	 * @return
+	 */
+	public double getX() {
+		return x;
+	}
+
+	/**
+	 * Get the current y coordinate
+	 * 
+	 * @return
+	 */
+	public double getY() {
+		return y;
+	}
+
+	/**
+	 * Give x a new value
+	 * 
+	 * @param x
+	 */
+	public void setX(double x) {
+		this.xPrev = getX();
+		this.x = x;
+	}
+
+	/**
+	 * Give y a new value
+	 * 
+	 * @param y
+	 */
+	public void setY(double y) {
+		this.yPrev = getY();
+		this.y = y;
+	}
+	
+	/**
+	 * Sets x, y to a new set of values
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void setXY(double x, double y) {
+		setX(x);
+		setY(y);
+	}
+
+	/**
+	 * Adds x coordinate
+	 * 
+	 * @param dx
+	 */
+	public void addX(double dx) {
+		this.xPrev = getX();
+		x += dx;
+	}
+
+	/**
+	 * Adds y coordinate
+	 * 
+	 * @param dy
+	 */
+	public void addY(double dy) {
+		this.yPrev = getY();
+		y += dy;
+	}
+
+	/**
+	 * Adds vector v to self
+	 * 
+	 * @param v
+	 */
+	public void addVector(Vector v) {
+		this.xPrev = getX();
+		this.yPrev = getY();
+		x += v.getX();
+		y += v.getY();
+	}
+
+	/**
+	 * @return the xPrev
+	 */
+	public double getxPrev() {
+		return xPrev;
+	}
+
+	/**
+	 * @return the yPrev
+	 */
+	public double getyPrev() {
+		return yPrev;
+	}
+
+	/**
+	 * @return the angle
+	 */
+	public double getAngle() {
+		return angle;
+	}
+
+	/**
+	 * @param angle the angle to set
+	 */
+	public void setAngle(double angle) {
+		this.angle = angle;
+	}
+}

095988736819c1a43b34a1bbf09595e5d1b06607
calisnelson <can18@duke.edu>
2016-04-07 16:57:39 -0400

    add volga and container

--- src/debug/ContainerArrayTest.java
+++ src/debug/ContainerArrayTest.java
@@ -44,4 +44,41 @@ public class ContainerArrayTest {
         myContainer.remove("Bear");
         assertEquals("Remove should be same reference", alligator, myContainer.get(0));
     }
+    
+    @Test
+    public void testAddTooMany(){
+    	for(int i=0; i<12; i++){
+    		myContainer.add("a" + i);
+    	}
+    	assertEquals(myContainer.get(11), null);
+    }
+    
+    @Test 
+    public void testRemove(){
+    	myContainer.add("bear");
+    	System.out.print(myContainer.get(0));
+    	myContainer.remove("bear");
+    	assertNotEquals(myContainer.get(0), "bear");
+    }
+    
+    @Test
+    public void testGet(){
+    	assertEquals(myContainer.get(12), null);
+    }
+    
+    @Test 
+    public void testRemoveNotThere(){
+    	int initialSize = myContainer.size();
+    	myContainer.remove("bear");
+    	assertEquals(initialSize, myContainer.size());
+    }
+    
+    @Test 
+    public void testCapacity(){
+    	for(int i=0; i<12; i++){
+    		myContainer.add("a" + i);
+    	}
+    	assertEquals(myContainer.size(), 10);
+    }
 }
+

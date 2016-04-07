712e04b7ccbf1b0339eeadbfd21ee781bf28c0df
ANNIE <anniekatelyntang@gmail.com>
2016-04-07 16:41:03 -0400

    1 & 2 DONE

--- src/debug/ContainerArray.java
+++ src/debug/ContainerArray.java
@@ -2,32 +2,48 @@ package debug;
 
 
 public class ContainerArray<E> {
-    private int initialCapacity = 10;
+    private static final int LIMIT = 10;
     private int currentSize = 0;
     private Object[] internalArray;
 
     public ContainerArray () {
-        this(10);
+        this(LIMIT);
     }
 
-    public ContainerArray (int initialCapacity) {
-        internalArray = new Object[initialCapacity];
+    public ContainerArray (int limit) {
+        internalArray = new Object[limit];
     }
 
-    public void add (E element) {
-        internalArray[currentSize++] = element;
+    public void add (E element) {			//1: handling array out of bounds; don't allow addition when full
+    	if(currentSize < internalArray.length){
+    		internalArray[currentSize++] = element;
+    	}
     }
 
     public int size () {
         return currentSize;
     }
 
-    public void remove (E objectToRemove) {
-        currentSize--;
+    public void remove (E objectToRemove) { //2: Remove object (first instance) and shift rightmost elements to left
+    	int index = -1;						
+    	for(int i = 0; i < internalArray.length; i++){
+    		if(objectToRemove.equals(internalArray[i])){
+    			index = i;
+    			break;
+    		}
+    	}
+    	if(index != -1){
+    		for(;index < internalArray.length-1; index++){
+    			internalArray[index] = internalArray[index+1];
+    			internalArray[index+1] = null;
+    		}
+    		currentSize--;		//3: only decrement currentSize if something is removed
+    	}
     }
 
     @SuppressWarnings("unchecked")
-    public E get (int index) {
-        return (E)internalArray[index];
+    public E get (int index) { //4: handle index out of bounds 
+    	if(index>=0 && index < LIMIT) return (E)internalArray[index];
+    	return null;
     }
 }
--- src/debug/ContainerArrayTest.java
+++ src/debug/ContainerArrayTest.java
@@ -20,27 +20,73 @@ public class ContainerArrayTest {
         myContainer.add("Camel");
         assertEquals("Add size", 3, myContainer.size());
     }
+    
+    @Test // 
+    public void testStoreLimit () {
+    	 myContainer.add("Alligator");
+         myContainer.add("Bear");
+         myContainer.add("Camel");
+         assertEquals("Add size", 3, myContainer.size());
+         myContainer.add("4");
+         myContainer.add("5");
+         myContainer.add("6");
+         myContainer.add("7");
+         myContainer.add("8");
+         myContainer.add("9");
+         myContainer.add("10");
+         myContainer.add("11");
+         myContainer.add("12");
+         myContainer.add("13");
+         myContainer.add("14");
+         assertEquals("Add size", 10, myContainer.size());
+    }
 
     @Test
     public void testObjectIsStored () {
         String alligator = "Alligator";
         myContainer.add(alligator);
+        myContainer.add("Alligator");
         assertEquals("Add should be same reference", alligator, myContainer.get(0));
     }
 
-    @Test
+    @Test 
     public void testSizeChangeWithRemove () {
         myContainer.add("Alligator");
         myContainer.add("Bear");
         myContainer.remove("Alligator");
         assertEquals("Remove size", 1, myContainer.size());
     }
+    
+    @Test //
+    public void testRemoveNonExistentObject () {
+    	myContainer.add("Alligator");
+        myContainer.add("Bear");
+        myContainer.remove("Falafel");
+        assertEquals("Remove size", 2, myContainer.size());
+    }
+    
+    @Test // 
+    public void testRemoveModifiesAndShiftsArray () {
+    	myContainer.add("Alligator");
+        myContainer.add("Bear");
+        myContainer.remove("Alligator");
+        assertEquals("Bear", myContainer.get(0));
+    }
 
+    @Test // 
+    public void testGetOutOfBounds () {
+    	myContainer.add("Alligator");
+        myContainer.add("Bear");
+        myContainer.add("Loofah");
+        assertEquals(null, myContainer.get(-1));
+        assertEquals(null, myContainer.get(2568));
+    }
     @Test
-    public void testObjectIsRemoved () {
+    public void testObjectIsRemoved () { 
         String alligator = "Alligator";
         myContainer.add("Alligator");
         myContainer.add("Bear");
+        myContainer.remove("Bear");
         myContainer.remove("Bear");
         assertEquals("Remove should be same reference", alligator, myContainer.get(0));
     }
